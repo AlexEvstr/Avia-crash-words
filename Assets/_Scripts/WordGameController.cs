56e44d273af648d150b6aea14b0bc92d28e834fb
Alexander Evstratov <alexdrugev@me.com>
2024-07-18 10:51:38 +0300

    Add obstacle and letter spawner.

--- Assets/_Scripts/WordGameController.cs
+++ Assets/_Scripts/WordGameController.cs
@@ -9,11 +9,17 @@ public class WordGameController : MonoBehaviour
     [SerializeField] private WordLevel[] _wordLevel;
     [SerializeField] private TMP_Text _currentWord;
     [SerializeField] private GameObject _win;
+    private ObstacleSpawner obstacleSpawner; // Ссылка на спаунер препятствий
+    [SerializeField] private float minDistance = 2.0f; // Минимальное расстояние между объектами
+    [SerializeField] private GameObject plane; // Ссылка на объект самолёта
     private HashSet<int> coloredIndices = new HashSet<int>();
     public static int LevelIndex;
 
+    private List<GameObject> spawnedLetters = new List<GameObject>();
+
     private void Start()
     {
+        obstacleSpawner = GetComponent<ObstacleSpawner>();
         LevelIndex = PlayerPrefs.GetInt("levelIndex", 1);
         Debug.Log(LevelIndex);
         _currentWord.text = _wordLevel[LevelIndex - 1].ShowWord();
@@ -22,9 +28,46 @@ public class WordGameController : MonoBehaviour
 
         foreach (var letter in letters)
         {
-            GameObject newLetter =  Instantiate(letter);
-            newLetter.transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-5, 5));
+            if (letter != null)
+            {
+                Vector2 newPosition = GenerateUniquePosition();
+                GameObject newLetter = Instantiate(letter, newPosition, Quaternion.identity);
+                spawnedLetters.Add(newLetter);
+            }
         }
+
+        // Инициализация спаунера препятствий с учетом уже существующих букв
+        obstacleSpawner.plane = plane; // Передача ссылки на самолёт
+        obstacleSpawner.Initialize(spawnedLetters, minDistance);
+    }
+
+    private Vector2 GenerateUniquePosition()
+    {
+        Vector2 newPosition;
+        bool positionIsValid;
+
+        do
+        {
+            newPosition = new Vector2(Random.Range(-30, 30), Random.Range(-30, 30));
+            positionIsValid = true;
+
+            foreach (var obj in spawnedLetters)
+            {
+                if (Vector2.Distance(newPosition, obj.transform.position) < minDistance)
+                {
+                    positionIsValid = false;
+                    break;
+                }
+            }
+
+            // Проверка на перекрытие с самолётом
+            if (positionIsValid && plane != null && Vector2.Distance(newPosition, plane.transform.position) < minDistance)
+            {
+                positionIsValid = false;
+            }
+        } while (!positionIsValid);
+
+        return newPosition;
     }
 
     public void UnlockNewLetter()
